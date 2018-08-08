a2841f0b63d16a2ce377a763d3a9e50c38a00a1e
harshil002 <34757900+harshil002@users.noreply.github.com>
2018-08-08 15:12:42 -0400

    Delete hi.py

--- hi.py
+++
@@ -1,9 +0,0 @@
-name = "jess"
-print(name)
-print(len(name))
-print("oye" + "SPAli")
-print(name.replace('j', 'm'))
-print(name.lower())
-print(name.upper())
-print("Count: ",name.count('s'))
-print(format(name))
