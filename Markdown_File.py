23c4fb716df93c764436a8e2376bdce856281636
Harshil Chaudhary <34757900+harshil002@users.noreply.github.com>
2018-08-14 15:57:38 -0400

    Add files via upload

---
+++ Markdown_File.py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+
+# GAME: ROCK, PAPER, Scissor  
+# 
+# This game includes rock, paper and scissor where the rock wins over scissor, scissor wins over paper and paper wins over rock.This game is about who will win in a random manner and only one user can paprticipate at a time versus computer.
+# for instance, let's suppose the player entered rock and the bot enters paper, here the bot wins the game.
+# This game is created using if,elseif,else statements, functions and library in python.This is a user based game where the useer has to input that one keywork in the form of rock or paper or scissor.
+# 
+# Challenges Faced/Proud moments:
+# 1.) I actually did not know how to make random generation of keywords possible. In meaning, how to make computer randomly choose     among rock, paper and scissor. I had to study about random library in python over the web and then I figured out it's usage.
+# 
+# 2.) Functions containing the if statements were something that played the trick on me. I was not able to particularly identify 
+#     that how to manage the statements within the functions.However, I managed it by rewriting some lines and debugging the code.
+# 
+# 3.) I was able to create a text based game where no computer was involved we can just enter two inputs and based on that the         program will provide that who wins rock or scissor or paper. But I learned about random generation and implemented the           concept.
+# 
+# 4.) From a technical standpoint I was really proud of implementing random keyword generation for computer.
+# 
+# 
+# INSRTUCTIONS to Play:
+# 
+# All text are case sensitive. User just has to enter a keyword:
+# 
+# Enter any keyword form the group of three. There are three keywords:
+# 1.) rock
+# 2.) paper
+# 3.) scissor
+# 
+# When the user enter the keywork and press enter the result will be displayed. One can play multiple times by running the program again and again.
